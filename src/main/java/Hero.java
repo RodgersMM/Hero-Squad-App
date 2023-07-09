810b6915aa5590c81be1df7af04137fbe2636ba6
rodgersmm <rodgersmutua44@gmail.com>
2023-07-09 12:38:50 +0300

    Add Hero Method

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -7,3 +7,12 @@ public class Hero {
     private String mWeakness;
     private int mId;
 }
+    // Add Hero Method
+    public Hero(String name,int age,String power,String weakness ) {
+        mName = name;
+        mAge = age;
+        mPower = power;
+        mWeakness = weakness;
+        heroes.add(this);
+        mId = heroes.size();
+    }
