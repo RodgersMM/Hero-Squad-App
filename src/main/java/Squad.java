55a305afab64aca1ca3baebc208e3c964407f168
rodgersmm <rodgersmutua44@gmail.com>
2023-07-09 13:08:50 +0300

    Adding Squad attributes

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -3,6 +3,13 @@ import java.util.ArrayList;
 
 
 public class Squad {
+    //Adding Squad attributes
+    private String mName;
+    private int mSize;
+    private String mCause;
+    private static List<Squad> instances = new ArrayList<Squad>();
+    private int mId;
+    private List<Hero> mHeroes;
 
 
 
