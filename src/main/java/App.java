3323e49e0dbe1813ea20ecb46610a4e36c98a3b2
rodgersmm <rodgersmutua44@gmail.com>
2023-07-09 12:11:38 +0300

    Route to show all squads in the squad list

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -57,6 +57,30 @@ public class App {
                 return new ModelAndView(model, layout);
             }, new VelocityTemplateEngine());
 
+            //Route to obtain input from the squad form after success message is displayed
+            post("/squads", (request, response) -> {
+                Map<String, Object> model = new HashMap<String, Object>();
+                String name = request.queryParams("name");
+                int size = Integer.parseInt(request.queryParams("size"));
+                String cause = request.queryParams("cause");
+                Squad newSquad= new Squad(name,size,cause);
+                model.put("template", "templates/squadSuccess.vtl");
+                return new ModelAndView(model, layout);
+            }, new VelocityTemplateEngine());
+
+            //Route to show all squads in the squad list
+            get("/squads", (request, response) -> {
+                Map<String, Object> model = new HashMap<String, Object>();
+                model.put("squads", Squad.all());
+                model.put("template", "templates/squads.vtl");
+                return new ModelAndView(model, layout);
+            }, new VelocityTemplateEngine());
+
+
+
+
+
+
 
 
 
