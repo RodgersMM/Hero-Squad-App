201d026ff1c1f11e9347e9a162f383eb0ec6cf75
rodgersmm <rodgersmutua44@gmail.com>
2023-07-09 12:37:26 +0300

    Add Hero.java class attributes

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,7 +1,9 @@
 import java.util. *;
 
 public class Hero {
-
-    
-
+    private String mName;
+    private int mAge;
+    private String mPower;
+    private String mWeakness;
+    private int mId;
 }
